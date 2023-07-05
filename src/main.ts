6383eca921e3a77c3f7ba9d7dfe9c5f435a08925
Mark_Maistrenko <revanmayfall@gmail.com>
2023-07-05 20:18:27 +0400

    ve

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const bot = app.get(getBotToken());
   app.use(bot.webhookCallback('/telegram'));
-
+  await bot.telegram.sendMessage(424027533, 'hello');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
