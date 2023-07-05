609932eb29fb9e2fb0349c3f1713ca017575d833
Mark_Maistrenko <revanmayfall@gmail.com>
2023-07-05 20:19:33 +0400

    v1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { getBotToken } from 'nestjs-telegraf';
 
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const bot = app.get(getBotToken());
+  const configService = app.get(ConfigService);
   app.use(bot.webhookCallback('/telegram'));
-  await bot.telegram.sendMessage(424027533, 'hello');
+  await bot.telegram.sendMessage(
+    424027533,
+    `hello from ${configService.get('DOMAIN')}`,
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
