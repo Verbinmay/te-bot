d33d8a8d9f07c767cc489a2d4b8436f0c8473e93
Mark_Maistrenko <revanmayfall@gmail.com>
2023-07-07 19:02:15 +0400

    v1.1

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
     abortOnError: false,
   });
   const bot = app.get(getBotToken());
